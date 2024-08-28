2d39422d292fe07e8af7cb0908906ecfdeace92c
Gene Grevemberg <unsound@gmail.com>
2024-08-28 12:44:40 -0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -4,29 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blogg</title>
-    <link rel="stylesheet" type="text/css" href="includes/styles.css">
+    <link rel="stylesheet" href="includes/styles.css">
 </head>
 <body>
 
     <div class="container">
-        <a href="index.php"><div class="avatar"></div></a>
-		<header><h1><a href="index.php">Neo's Hideout</a></h1></header>
-        <div class="menu">
-		    <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <!--<li><a href="post.php">Post</a></li>-->
-                <li><a href="#" id="toggleLink">Search</a></li>
-            </ul>
-        </div>
+    <a href="index.php"><h2 class="title">[<span class="green">tone</span>blogg]</h2></a>
+    <nav>
+        <ul class="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+           <li><a href="#" id="toggleLink">Search</a></li>
+        </ul>
+    </nav>
         <div id="collapsibleDiv" class="collapsed">
         <div class="search">
         <form method="GET" action="index.php">
-            <input type="text" name="search" placeholder="" class="searchinput" value="\\\">
+            <input type="text" name="search" placeholder="" class="searchinput" value="">
             <button type="submit">Search</button>
         </form>
         </div>
 		</div>
-        <main>
-            <section>
