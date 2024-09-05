feda7ba19a6b27af26b43870e0ba60c098ccd0cd
Gene Grevemberg <unsound@gmail.com>
2024-09-05 18:31:12 -0500

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,19 @@
-            </section>
-        </main>
         <footer>
-            <p>&copy; 2024 Your Website. All rights reserved.</p>
+            <p>&copy; 2024 toneblogg. All rights reserved.</p>
         </footer>
     </div>
-    
-<script src="script.js"></script>
+<script>
+document.getElementById('toggleLink').addEventListener('click', function(event) {
+    event.preventDefault();
+    var collapsibleDiv = document.getElementById('collapsibleDiv');
+    if (collapsibleDiv.classList.contains('expanded')) {
+        collapsibleDiv.classList.remove('expanded');
+        collapsibleDiv.classList.add('collapsed');
+    } else {
+        collapsibleDiv.classList.remove('collapsed');
+        collapsibleDiv.classList.add('expanded');
+    }
+});
+</script>
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
             <li><a href="index.php">Home</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact</a></li>
-           <li><a href="#" id="toggleLink">Search</a></li>
+            <li><a href="#" id="toggleLink">Search</a></li>
         </ul>
     </nav>
         <div id="collapsibleDiv" class="collapsed">
