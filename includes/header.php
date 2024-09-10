299a7d3f8535f5b4a904f4ddb2a58bab785d1800
Gene Grevemberg <unsound@gmail.com>
2024-09-10 14:44:46 -0500

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -9,13 +9,20 @@
 <body>
 
     <div class="container">
+    <header class="video-header">
+        <video autoplay muted loop class="video-bg">
+            <source src="https://i.imgur.com/KuhvPdZ.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+        </video>
+
     <a href="index.php"><h2 class="title">[<span class="green">tone</span>blogg]</h2></a>
+    <a href="#" id="toggleLink"><img id="searchbutton" src="search.png" /></a>
     <nav>
         <ul class="menu">
-            <li><a href="index.php">Home</a></li>
+            <li><a href="index.php">blogg</a></li>
             <li><a href="about.php">About</a></li>
             <li><a href="contact.php">Contact</a></li>
-            <li><a href="#" id="toggleLink">Search</a></li>
+           <li><a href="table.php">Archive</a></li>
         </ul>
     </nav>
         <div id="collapsibleDiv" class="collapsed">
@@ -26,3 +33,5 @@
         </form>
         </div>
 		</div>
+    </header>
+
