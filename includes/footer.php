25602a28458a54404f10361c2582d6df819121c6
Gene Grevemberg <unsound@gmail.com>
2024-09-13 16:45:08 -0500

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -15,5 +15,11 @@ document.getElementById('toggleLink').addEventListener('click', function(event) 
     }
 });
 </script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css">
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
+<script>hljs.highlightAll();</script>
 </body>
 </html>
